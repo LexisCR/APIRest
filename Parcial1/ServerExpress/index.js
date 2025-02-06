b810c5361dade38f75b3708cedcd4ed8e5876144
Alex <alejandrocortina1@gmail.com>
2025-02-05 23:56:06 -0600

    Http

--- Parcial1/ServerExpress/index.js
+++ Parcial1/ServerExpress/index.js
@@ -3,7 +3,12 @@ const app=express();
 const port=3000;
 
 app.get('/', (req,res,next)=>{
+    res.header('Access-Control-Allow-Origin', '*')
     res.sendFile(__dirname+'/index.html');
+})
+
+app.post('/', (req, res) =>{
+    res.send('Hello world');
 })
 
 app.listen(port,()=>{
