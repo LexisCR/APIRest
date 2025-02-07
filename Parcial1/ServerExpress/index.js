8dd626adea276c047c0e7b0c4321cc95ba3a5b82
Alex <alejandrocortina1@gmail.com>
2025-02-07 17:59:12 -0600

    Middleware

--- Parcial1/ServerExpress/index.js
+++ Parcial1/ServerExpress/index.js
@@ -1,17 +1,41 @@
 const express=require('express');
 const app=express();
 const port=3000;
+ 
+/*Middlewawre de Aplicacion
+app.use('/',(req,res,next)=>{
+console.log("Peticion al server");
+next();
+}, (req,res,next)=>{
+    console.log("2da funcion middlewawre");
+    next();
+}
+);
+ */
 
-app.get('/', (req,res,next)=>{
-    res.header('Access-Control-Allow-Origin', '*')
+// Middleware incorporado en express
+app.use(express.json());
+app.use(express.text());
+ 
+app.get('/alumnos', (req,res,next)=>{
+    console.log(req.query);
     res.sendFile(__dirname+'/index.html');
 })
-
-app.post('/', (req, res) =>{
-    res.send('Hello world');
+ 
+app.post('/sistemas/:control', (req,res)=>{
+    console.log(req.params)
+    res.send("HOLA MUNDO");
 })
 
+app.get('/maestros', (req,res)=>{
+    console.log(req.body)
+    res.send("Hola Mundo!");
+})
+ 
+app.use('/', (req,res,next)=>{
+    res.status(404).send("Error 404");
+})
+ 
 app.listen(port,()=>{
-    console.log('Server running at http://localhost:3000');
+console.log('Server running at http://localhost:3000');
 });
-
--- Parcial1/ServidorHttp/index.js
+++ Parcial1/ServidorHttp/index.js
@@ -1,11 +1,11 @@
 var http = require('http');
 
 let server = http.createServer(function (req, res) {
-  res.writeHead(200, {'Access-Control-Allow-Origin': '*'});
-  //res.writeHead(200, {'Content-Type': 'text/plain'});
+  //res.writeHead(200, {'Access-Control-Allow-Origin': '*'});
+  res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('hello world!');
 })
 
-server.listen(3001, ()=> {
-console.log("Servidor Http corriendo en pto 3001")
+server.listen(3000, ()=> {
+console.log("Servidor Http corriendo en pto 3000")
 });
