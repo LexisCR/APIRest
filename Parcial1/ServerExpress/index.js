f4d5b0f605c443d0879184d1ef9f294de1926e27
Alex <alejandrocortina1@gmail.com>
2025-02-14 23:57:51 -0600

    XML y Formularios

--- Parcial1/ServerExpress/index.js
+++ Parcial1/ServerExpress/index.js
@@ -1,7 +1,10 @@
 const express=require('express');
+const xmlparser = require('express-xml-bodyparser');
+const multer = require('multer');
+const path = require('path');
 const app=express();
 const port=3000;
- 
+
 /*Middlewawre de Aplicacion
 app.use('/',(req,res,next)=>{
 console.log("Peticion al server");
@@ -16,7 +19,14 @@ next();
 // Middleware incorporado en express
 app.use(express.json());
 app.use(express.text());
- 
+app.use(xmlparser()); 
+
+
+const folder = path.join(__dirname+'/archivos/'); //Definir una ruta para almacenar archivos que se envian del cliente
+const upload = multer({dest:folder}); //Pasarle esa ruta a multer()
+app.use(upload.single('archivo')); 
+//app.use(upload.none()); 
+
 app.get('/alumnos', (req,res,next)=>{
     console.log(req.query);
     res.sendFile(__dirname+'/index.html');
@@ -32,6 +42,16 @@ app.get('/maestros', (req,res)=>{
     res.send("Hola Mundo!");
 })
  
+app.post('/prefectos', (req,res)=>{
+    //console.log(req.body)
+    //res.send("Hola Mundo!");
+
+    console.log(`Se recibio el archivo: ${req.file.originalname}`);
+    console.log('Se recibio el formulario:'+JSON.stringify(req.body));
+    res.json(req.body);
+
+})
+
 app.use('/', (req,res,next)=>{
     res.status(404).send("Error 404");
 })
